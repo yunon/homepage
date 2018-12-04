6f33648bf2220aa0d3eca419eff7e567de2a13d9
yunon <purple2000tatu@yahoo.co.jp>
2018-12-04 23:52:26 +0900

    tag登録処理

--- html/sqlite/receive.php
+++ html/sqlite/receive.php
@@ -1,88 +1,69 @@
+<?php
 
-<!-- フォームからの入力を受け取ってデータベースに登録する -->
+  $sourcecode = $_POST["sourcecode"];
+  $title = $_POST["title"];
 
-<!doctype html>
-<html lang="ja">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  try {
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO('sqlite:sourcedata.sqlite','' ,'' ,$options);
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    // ---  重複しない10桁のランダムな英数字を作成 ---------------
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-
-    <div class="container">
+    $str_r;
+    $sts = true;
+    while($sts){
+      // 10桁のランダムな英数字を取得
+      $str = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ";
+      $str_r = substr(str_shuffle($str), 0, 10);
       
-      <h1> test </h1>
+      // idが一致するレコードの数を出力するSQL文
+      $sql1 = "SELECT COUNT(*) AS 'count' FROM sourcedata WHERE id = '".$str_r."';";
 
-      <?php
+      $result1 = $pdo->query($sql1);  
+      
+      foreach($result1 as $row){
+        if($row["count"] == 0) $sts = false;
+      }    
+    }
 
-          $sourcecode = $_POST["sourcecode"];
-          $title = $_POST["title"];
+    // タグを分けて配列に保存
+    $arrTg= preg_split("/[\s,]+/", $_POST["tag"]);
 
-          try {
-            $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-            $pdo = new PDO('sqlite:sourcedata.sqlite','' ,'' ,$options);
+    foreach($arrTg as $i){
+      
+      $sql1 = "SELECT COUNT(*) AS 'count' FROM tagdata WHERE tagname='".$i."';";
+      $result1 = $pdo->query($sql1);
+      foreach($result1 as $row){
+        if($row["count"] == 0){
+          $pdo->query("INSERT INTO tagdata(tagname) VALUES('".$i."');");
+        }
+      }
+      $result1 = $pdo->query("SELECT tagID FROM tagdata WHERE tagname='".$i."';");
+      foreach($result1 as $i){
+        $res = $i["tagID"];
+      }
+      $st = $pdo->prepare("INSERT INTO tag VALUES(:id, :tagID);");
+      $st->bindParam(":id", $str_r);
+      $st->bindParam(":tagID", $res);
+      $st->execute();
+    }
+    
+    // ---------- sqliteにデータを追加 -----------------
+    
+    $sql1 = "INSERT INTO sourcedata(id, title) VALUES(:id, :title);";
+    $st = $pdo->prepare($sql1);
+    $st->bindParam(':id', $str_r);
+    $st->bindParam(':title', $title);
+    $st->execute();
 
+    // ファイル作成
+    $dir = "../sourcecode/";
+    file_put_contents($dir.$str_r.".html" ,$sourcecode);
 
-            // ---  重複しない10桁のランダムな英数字を作成 ---------------
+    header("Location: ../index.html");
 
-            $str_r;
-            $sts = true;
-            while($sts){
-              // 10桁のランダムな英数字を取得
-              $str = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ";
-              $str_r = substr(str_shuffle($str), 0, 10);
-              
-              // idが一致するレコードの数を出力するSQL文
-              $sql1 = "SELECT COUNT(*) AS 'count' FROM sourcedata WHERE id = '".$str_r."';";
-
-              $result1 = $pdo->query($sql1);  
-              
-              foreach($result1 as $row){
-                if($row["count"] == 0) $sts = false;
-              }    
-            }
-
-
-            // ---------- sqliteにデータを追加 -----------------
-
-            $sql1 = "INSERT INTO sourcedata(id, title) VALUES(:id, :title);";
-            $result1 = $pdo->prepare($sql1);
-            $result1->bindParam(':id', $str_r);
-            $result1->bindParam(':title', $title);
-            $result1->execute();
-
-            
-            // ファイル作成
-            $dir = "../sourcecode/";
-            file_put_contents($dir.$str_r.".html" ,$sourcecode);
-
-  
-            header("Location: ../index.html");
-
-            
-
-            echo "sucessfull!! <br>";
-            echo "タイトル : ", $title, "<br>";
-            //echo "ファイル名 : ", $str_r, "<br>";
-
-          } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
-            exit;
-          }         
-      ?>
-
-    </div>
-  
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-</html>
+  } catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
+  }         
+?>
