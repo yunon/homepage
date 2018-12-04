78da0c9b2d1dcd80aa9227c13cbbac28982cd47b
yunon <purple2000tatu@yahoo.co.jp>
2018-12-04 20:52:16 +0900

    ソートと検索機能の追加

--- html/sqlite/receive_js.php
+++ html/sqlite/receive_js.php
@@ -3,15 +3,28 @@
     header('Content-type: application/json ');
 
     try{
-        if(@$_POST['num'] == "") $num = 6;
-        else $num = @$_POST['num'];
+        
+        $num = @$_POST['num'];
+
+        $filter = $_POST["filter"];
+        $colname = "create_time";
+        $word = $_POST["word"];
+
+        if($filter == "new"){
+            $filter = "DESC";
+        }else if($filter == "old"){
+            $filter = "ASC";
+        }else{
+            $colname = "title";
+            $filter = "ASC";
+        }
 
         $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
 
         $pdo = new PDO("sqlite:sourcedata.sqlite", "", "", $options);
 
-        $result = $pdo->query("SELECT * FROM sourcedata ORDER BY create_time DESC LIMIT ".$num.", 5".";")->fetchAll(PDO::FETCH_ASSOC);
-        
+        $result = $pdo->query("SELECT * FROM sourcedata WHERE title LIKE '%".$word."%' ORDER BY ".$colname." ".$filter." LIMIT ".$num.", 5;")->fetchAll(PDO::FETCH_ASSOC);
+    
         // jsonエンコード
         echo json_encode($result);
 
