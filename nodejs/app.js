3ae99dc346c117ef1c4c68a204c25751c3b90c51
unknown <183107@lab-ocs.local>
2019-01-29 14:54:06 +0900

    ejsとcookieを使ってみた

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,10 +2,19 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var ejs = require('ejs');
 
-//POSTでリクエストを取得するのに必要
+// cookie-parser(クッキーを利用する際に必要)
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// body-parser(POSTでリクエストを取得するのに必要)
 var bodyParser = require('body-parser');
 app.use(bodyParser());
+
+// テンプレートエンジンはEJSを使うよと宣言？
+app.engine('ejs', ejs.renderFile);
+
 
 var database = require('./routes/database');
 
@@ -16,4 +25,18 @@ app.listen(3000);
 app.use(express.static('public'));
 
 app.use('/database', database);
+
+// 試しにクッキーを使ってみる
+app.get('/test_cookie',function(req, res){
+    // クッキーの取得
+    var cnt = req.cookies.cnt == undefined ? 0 : req.cookies.cnt;
+    cnt++;
+
+    // クッキーの送信
+    res.cookie('cnt',cnt,{maxAge:60000});
+    // EJSを返す
+    res.render('temp.ejs',{
+        cnt: cnt
+    });
+});
 
