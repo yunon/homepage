7ae336d0e47e2fc0b25d5169f30a6cc265b2959f
米澤辰晃 <yonezawashinakira@yonezawashinakiranoMacBook-ea.local>
2019-02-06 01:43:16 +0900

    request test

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,10 +1,23 @@
 var assert = require('assert');
 var myModule = require('./routes/login.js');
-var http = require('http');
+//requestをrequire
+var request = require('request');
 
-http.get('http://localhost:3000',function(res){
-    res.setEncoding('utf8');
-    res.on('data',(data)=>{
-        console.log(data);
-    })
+//ヘッダーを定義
+var headers = {
+  'Content-Type':'application/json'
+}
+
+//オプションを定義
+var options = {
+  url: 'http://localhost:3000/login',
+  method: 'POST',
+  headers: headers,
+  json: true,
+  form: { name : "tatuki", pass:"unk03", redirect : "/"},
+}
+
+//リクエスト送信
+request('http://localhost:3000/login', function (error, res, body) {
+  console.log(body)
 })
